50f85891b362ab15a290107c179fdef07dd9bfe5
Seb0927 <sebastianidrobo@hotmail.com>
2023-06-28 21:20:26 -0500

    Actualización de la nueva baseURL

--- src/api/article.api.js
+++ src/api/article.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const articleApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/inventory/'
+    baseURL: 'https://lithium-backend.onrender.com/inventory/'
 });
 
 export const getAllCars = () => {
--- src/api/login.api.js
+++ src/api/login.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const loginApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/login/'
+    baseURL: 'https://lithium-backend.onrender.com/login/'
 });
 
 export const getCustomers = () => {
